3856caf856ee0e0ff0e73043c0e790252993c177
Georgi Mumdzhiev <george.mumdzhiev@gmail.com>
2018-05-14 14:04:46 +0200

    adding some things

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { PureComponent } from 'react';
 import Hangman from './Hangman';
 import { done, randomWord, renderWord } from './words';
+import Style from './style.css';
 
 // This shows you how to use the functions in words.js:
 /*
@@ -14,13 +15,25 @@ class App extends PureComponent {
     super()
     this.state = {
       progress: 0,
+      word: randomWord(),
+      guesses: [],
     }
   }
 
   render() {
     const {
       progress,
+      word,
+      guesses,
     } = this.state
+    /*  map(letter => <button onClick={() => this.seState({ letter }) > A</button>) */
+    if (done(word, guesses)) {
+      return (
+        <div>YOU WON!
+          Congrats
+      </div>
+      )
+    }
 
     return (
       <div>
@@ -28,13 +41,28 @@ class App extends PureComponent {
           {/* This is how we render the hanging man */}
           <Hangman progress={progress} />
         </div>
+        <input type="text" value={renderWord(word, guesses)} />
+        {'abcdefghijklmnopqrstuvwxyz'.split('').map(letter => this.renderInputButton(letter))}
+        <p>{guesses}</p>
 
-        <button onClick={() => this.setState({ progress: progress + 1 })}>
-          Hang The Man !
-        </button>
       </div>
     )
   }
+  renderInputButton(letter) {
+    const {
+      progress,
+      word,
+      guesses
+    } = this.state
+    return (
+      <button onClick={() => this.setState({
+        guesses: [letter, ...guesses],
+        progress: word.indexOf(letter) > -1 ? progress : progress + 1,
+        // word: done(word, guesses)  == word ? alert("YOU WON") : word
+      })}>
+        {letter}
+      </button>
+    )
+  }
 }
-
 export default App;
